6ddad281ceb4636074d3c616c76cfd2352f3c70b
ellomarshamllow <me@eleonorarenz.com>
2021-12-10 18:23:56 +0100

    Add SpeedRadar functionality

--- src/main/java/master/SpeedRadar.java
+++ src/main/java/master/SpeedRadar.java
@@ -1,18 +1,52 @@
 package master;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 
 public class SpeedRadar {
-    static int threshold = 90;
+    static final int SPEED_LIMIT = 90;
 
     // Detect cars which go over the speed limit of 90
     public static SingleOutputStreamOperator detectSpeeding(
-            SingleOutputStreamOperator<VehicleReport> tuples) {
-        // return tuples.filter(a -> a >= threshold);
-        return tuples;
+            SingleOutputStreamOperator<VehicleReport> output) {
+        return output.filter(
+                        new FilterFunction<VehicleReport>() {
+                            @Override
+                            public boolean filter(VehicleReport v) throws Exception {
+                                if (v.getSpeed() > SPEED_LIMIT) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
+                            }
+                        })
+                // We convert into KeyStream to use the same report
+                .keyBy(
+                        new KeySelector<VehicleReport, Tuple2<Integer, Integer>>() {
+
+                            @Override
+                            public Tuple2<Integer, Integer> getKey(VehicleReport v) {
+                                return Tuple2.of(v.getVid(), v.getDirection());
+                            }
+                        })
+                .map(
+                        new MapFunction<VehicleReport, SpeedRadarReport>() {
+                            SpeedRadarReport sre = new SpeedRadarReport();
+
+                            @Override
+                            public SpeedRadarReport map(VehicleReport vehicleReport)
+                                    throws Exception {
+                                sre.setTime(vehicleReport.getTime());
+                                sre.setVid(vehicleReport.getVid());
+                                sre.setHighway(vehicleReport.getHighway());
+                                sre.setSegment(vehicleReport.getSegment());
+                                sre.setDirection(vehicleReport.getDirection());
+                                sre.setSpeed(vehicleReport.getSpeed());
+                                return sre;
+                            }
+                        });
     }
 }
-//     compute the sum of the tips per hour for each driver
-//    DataStream<Tuple3<Long, Long, Float>> hourlyTips = fares
-//            .keyBy((TaxiFare fare) -> fare.driverId)
-//            .process(new PseudoWindow(Time.hours(1)));
